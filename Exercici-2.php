021e4316bf75c0e56ec3787972ed2d2996395a24
MitchelRos <mrosado19@jaumebalmes.net>
2022-09-22 20:48:29 +0200

    añadir si esta completado o no

--- Exercici-2.php
+++ Exercici-2.php
@@ -5,18 +5,30 @@
 <!-- C:\xampp\htdocs  crea carpeta i extra er localhost:80/newPhpProject-->
 <?php
 $taules = array();
+$estado = "";
 for ($i = 0; $i < 10; $i++) {
     $numeroComensal = rand(0, 5);
-    array_push($taules, $numeroComensal);
+    array_push($taules, $numeroComensal,$estado);
 }
 ?>
-<h1> <?php
+    <h1>
+        <?php
 
-    for ($i = 0; $i < count($taules); $i++) {
-        echo "Taula ",$i+1, " Comensal: ", $taules[$i],"<br>";
-    }
+            for ($i = 0; $i < count($taules); $i++) {
+                if ($numeroComensal==5) {
+                    $estado = " - Completo";
+                } else {
+                    if ($numeroComensal==0) {
+                        $estado = " - Vacio";
+                    } else{
+                        $estado= " ";
+                    }
+                }
+                echo "Taula ",$i+1, " Comensal: ", $taules[$i], $estado,"<br>";
+            }
 
- ?></h1>
+        ?>
+    </h1>
 </body>
 </html>
 
